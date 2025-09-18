5e252ad811ca34679b9b55312f3fc69cc643bfed
Thiha Swan Htet <thihash18.23@ichat.sp.edu.sg>
2025-09-18 11:23:37 +0800

    Added login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Features Routes
 import { Quiz } from "./features/quiz/components/quiz";
+import { Login } from "./features/auth/components/login";
 
 export function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Quiz />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </Router>
   );
