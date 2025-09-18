c0a8e56b4dc8f999e6569177448cf1794231b786
Thiha Swan Htet <thihash18.23@ichat.sp.edu.sg>
2025-09-18 11:26:30 +0800

    comment(): Router app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Quiz } from "./features/quiz/components/quiz";
 import { Login } from "./features/auth/components/login";
 import { Inventory } from "./features/inventory/components/inventory";
-import { Journaling } from "./features/journaling/components/Journaling";
+// import { Journaling } from "./features/journaling/components/journaling";
 
 export function App() {
   return (
@@ -13,7 +13,7 @@ export function App() {
         <Route path="/" element={<Quiz />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/inventory" element={<Inventory />}></Route>
-        <Route path="/journaling" element={<Journaling />}></Route>
+        {/* <Route path="/journaling" element={<Journaling />}></Route> */}
       </Routes>
     </Router>
   );
